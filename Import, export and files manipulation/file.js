799760ae3d6c754b929e0670d4eb973a58482557
mahii0809 <42196047+mahii0809@users.noreply.github.com>
2020-09-09 14:01:28 +0530

    Delete file.js

--- Import, export and files manipulation/file.js
+++
@@ -1,5 +0,0 @@
-var file=require('fs');
-file.writeFileSync("./demo.txt","kiran123");
-//file.appendFileSync("./demo.txt","mahe0809");
-var data=file.readFileSync('./demo.txt',{encoding:'utf-8'});
-console.log(data.toString());
