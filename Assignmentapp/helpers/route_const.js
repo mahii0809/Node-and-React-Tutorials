641eccecbcaa925ba4712e2d6d20d7850ed99e82
mahii0809 <42196047+mahii0809@users.noreply.github.com>
2020-09-09 21:38:04 +0530

    assignment

---
+++ Assignmentapp/helpers/route_const.js
@@ -0,0 +1,8 @@
+module.exports.ROUTE_CONSTANTS={
+    DEFAULT:"/",
+    GET_PRODUCTS:"/userview",
+    GET_LOGIN:"/login",
+    GET_REGISTER:"/registration",
+    POST_LOGIN:"/login",
+    POST_REGISTER:"/registration"
+}
